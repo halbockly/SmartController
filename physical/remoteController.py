71eb794ea89086a1f9f95db2a7581cb8987788d1
gauman061 <gauman323>
2019-09-01 11:45:31 +0900

    手直しその２

--- physical/remoteController.py
+++ physical/remoteController.py
@@ -1,18 +1,31 @@
 from bottle import Bottle, run, route, abort, request
-import physical.adrsirlibmaster.adrsirlib as ir
+# import adrsirlib as ir
 
 import json
 
 app = Bottle()
 
 
-# リモートコントローラークラス
+# リモコン基盤操作クラス
 class remoteController():
 
-    # リモコン家電操作メソッド
-    def execute(self, kaden_id, operation_id):
-        # 赤外線.jsonの読み出し
-        infraRed = ir.send("")
+    # 操作メソッド
+    def execute(self, kadenId, operationId):
+        result = True
+
+        try:
+            # とりあえずこれが動けば成功
+            print('家電ID:' + kadenId + ' 操作ID：' + operationId)
+
+            # 実機じゃないと動かないのでコメントアウト。そのうちブランチ切る
+            # 赤外線.jsonの読み出し
+            # infraRed = ir.send("")
+
+        except:
+            result = False
+
+        return result
+
 
     # 赤外線信号の読み出しメソッド（Privateにしたい。
     def getInfraRedSign(self, kaden_id, operation_id):
