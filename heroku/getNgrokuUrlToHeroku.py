50b1888ec77c4c94c249b5a20bbacb40fc2f317f
gauman061 <gauman061@gmail.com>
2019-11-13 10:10:31 +0900

    ソースの反映先おもっくそ間違えてたので修正してコミット

--- heroku/getNgrokuUrlToHeroku.py
+++ heroku/getNgrokuUrlToHeroku.py
@@ -1,54 +1,20 @@
-import subprocess
-import time
+
+from bottle import Bottle, run, route, abort, request
+# とりあえずのサンプルソース（動作確認済み
 import configparser
-import requests
 
-# ----------------------------------------------------
-# Herokuに対してngrokのURLを送る為の部品
-# 再起動時デーモンから呼び出してもらう。
-#
+app = Bottle()
 
-cmd = './ngrok http 8080 --log=stdout'
+@app.route('/getNgrokuUrlToHeroku', method='POST')
+def GetNgrokuUrlToHeroku():
 
-from subprocess import Popen, PIPE
+    body = request.body
+    text_body = body.read().decode('UTF-8')
+    print(text_body)
 
+    inifile = configparser.ConfigParser()
+    inifile.read("./settings/ngrokToHeroku.ini")
+    inifile.set("ngrok", "url", text_body)
 
-# ngrokのSTDOUTに下記が現れたらURLが出力される。
-__STARTED_TUNNEL__ = "started tunnel"
-# 下記の値がURL
-__ADDR__ = "addr="
-# 取り出したURLの送信先
-__REQUEST_URL__ = "http://localhost:8080/getNgrokuUrlToHeroku"
-
-url = ""
-
-# ngrokのEXE?を起動する。起動待ちとして1秒待つ
-p = Popen(cmd.split(' '), stdout=PIPE, stderr=PIPE)
-time.sleep(1)
-
-# pの中にSTDOUTへのPIPEが渡されるので
-# そこからリアルタイムで1行ずつ読み込み→解析する。
-for line in iter(p.stdout.readline, b''):
-
-    decodeLine = line.rstrip().decode("utf8")
-
-    # STDOUT内にstarted tunnel　が現れたら
-    msgIndex = decodeLine.find(__STARTED_TUNNEL__)
-    print(decodeLine)
-
-    # その行の addr= 以降がURLになる
-    if (msgIndex != -1):
-        addrIndex = decodeLine.find(__ADDR__)
-        url = decodeLine[addrIndex + len(__ADDR__):]
-
-        print(url)
-
-        #URLを送信する。※未検証　でもPOSTを送信するメソッドが.postなのはわかりやすくていいね。
-        response = requests.post(__REQUEST_URL__,data=url)
-        break
-
-# このメソッドが終わるとngrokの起動状態も破棄されるのでとりあえず回しておく。
-# 何かheartbeatとか終了SEQとかここに入れておくといいかも
-while (True):
-    time.sleep(1)
+    return {'statusCode': 200, 'body': '{}'}
 
--- setNgrokUrlToHeroku.py
+++ setNgrokUrlToHeroku.py
@@ -1,13 +1,54 @@
-from bottle import Bottle, run, route, abort, request
-
+import subprocess
+import time
 import configparser
+import requests
 
-app = Bottle()
+# ----------------------------------------------------
+# Herokuに対してngrokのURLを送る為の部品
+# 再起動時デーモンから呼び出してもらう。
+#
 
-# -------------------------------------------
-#   ngrokのURLをHerokuに送る
+cmd = './ngrok http 8080 --log=stdout'
+
+from subprocess import Popen, PIPE
 
 
+# ngrokのSTDOUTに下記が現れたらURLが出力される。
+__STARTED_TUNNEL__ = "started tunnel"
+# 下記の値がURL
+__ADDR__ = "addr="
+# 取り出したURLの送信先
+__REQUEST_URL__ = "http://localhost:8080/getNgrokuUrlToHeroku"
 
+url = ""
 
+# ngrokのEXE?を起動する。起動待ちとして1秒待つ
+p = Popen(cmd.split(' '), stdout=PIPE, stderr=PIPE)
+time.sleep(1)
+
+# pの中にSTDOUTへのPIPEが渡されるので
+# そこからリアルタイムで1行ずつ読み込み→解析する。
+for line in iter(p.stdout.readline, b''):
+
+    decodeLine = line.rstrip().decode("utf8")
+
+    # STDOUT内にstarted tunnel　が現れたら
+    msgIndex = decodeLine.find(__STARTED_TUNNEL__)
+    print(decodeLine)
+
+    # その行の addr= 以降がURLになる
+    if (msgIndex != -1):
+        addrIndex = decodeLine.find(__ADDR__)
+        url = decodeLine[addrIndex + len(__ADDR__):]
+
+        print(url)
+
+        #URLを送信する。※未検証　でもPOSTを送信するメソッドが.postなのはわかりやすくていいね。
+        response = requests.post(__REQUEST_URL__,data=url)
+        break
+
+# このメソッドが終わるとngrokの起動状態も破棄されるのでとりあえず回しておく。
+# 何かheartbeatとか終了SEQとかここに入れておくといいかも
+while (True):
+    time.sleep(1)
 
