d74eff46468887c064b34d6fc8faa871e43b836a
gauman061 <gauman061@gmail.com>
2020-03-22 13:22:19 +0900

    本番環境とすり合わせ

--- util/Log.py
+++ util/Log.py
@@ -5,6 +5,7 @@ import logging
 #   あんまりindex.pyに手を入れるのもあれなのでファイルを分けた。
 #
 def initLog():
+    print("ログスタート")
     format = "%(asctime)s [%(levelname)s] %(message)s [%(filename)s.%(funcName)s(%(lineno)s)]"
     logging.basicConfig(filename='./log/SmartController.log', format=format, level=logging.INFO)
 
